b1760f15fa5ecda84bdd287d4f356cd8c3ed1d43
Fabian Varela Bonett <jfabian.303@gmail.com>
2018-09-01 15:23:39 -0500

    Initialize the project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RedisDemoAppDotNetCore
 Demo using Azure Redis Cache with dot net core
+
+Quick start here => https://docs.microsoft.com/es-es/azure/redis-cache/cache-dotnet-core-quickstart
---
+++ RedisConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RedisConsole
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
